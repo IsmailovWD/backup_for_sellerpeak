9a3636148fcb9fc28f51f902374fce94f5b38240
ismailovWD <ismailovWD@gmail.com>
2024-07-02 09:06:22 +0500

    com

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import config from './firebaseConfig.js';
 import { initializeApp } from 'firebase/app';
 import { getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config()
@@ -8,6 +9,7 @@ dotenv.config()
 initializeApp(config)
 const SERVER_NAME = process.env.SERVER_NAME;
 const storage = getStorage()
+const auth = getAuth();
 
 const giveCurrentDateTime = () => {
     const today = new Date();
@@ -18,17 +20,28 @@ const giveCurrentDateTime = () => {
 }
 
 export async function uploadFunc(filePath) {
-    const dateTime = giveCurrentDateTime();
-    const storageRef = ref(storage, `files/${dateTime}.zip`);
-    const metadata = {
-        contentType: 'application/zip',
-    };
-
-    fs.readFile(filePath, async(err, data) => {
-        const stub = await uploadBytesResumable(storageRef, data, metadata).then(() => {
-            console.log("File uploaded successfully");
+    try{
+        const email = process.env.EMAIL;
+        const password = process.env.PASSWORD;
+        await signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
+            console.log("User signed in successfully");
         }).catch((error) => {
             console.log(error);
         })
-    })
+        const dateTime = giveCurrentDateTime();
+        const storageRef = ref(storage, `files/${dateTime}.zip`);
+        const metadata = {
+            contentType: 'application/zip',
+        };
+    
+        fs.readFile(filePath, async(err, data) => {
+            const stub = await uploadBytesResumable(storageRef, data, metadata).then(() => {
+                console.log("File uploaded successfully");
+            }).catch((error) => {
+                console.log(error);
+            })
+        })
+    }catch(e){
+        console.log(e);
+    }
 }
